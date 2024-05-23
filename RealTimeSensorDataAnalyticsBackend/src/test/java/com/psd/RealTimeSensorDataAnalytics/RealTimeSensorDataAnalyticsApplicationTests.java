3f40ae9dac3479221bd27698175146a7eb9cfe81
LaxminarayanaV7416 <laxminarayana.vadnala@gmail.com>
2024-05-23 15:39:16 -0500

    added spring initializer project

---
+++ RealTimeSensorDataAnalyticsBackend/src/test/java/com/psd/RealTimeSensorDataAnalytics/RealTimeSensorDataAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psd.RealTimeSensorDataAnalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeSensorDataAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
