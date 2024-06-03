4b8b832adad2cc29330c7d7ade01ecd9b045678f
Sai Dinesh Vasireddy <saidinesh.vasireddy@slu.edu>
2024-06-02 22:21:30 -0500

    FP-22,23,6,17 Created a dashboard with static machine count and graph which can later be linked to backend

---
+++ sensor_analytics_frontend/src/Dashboard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import NavBar from './Components/NavBar';
+import SideBar from './Components/SideBar';
+import Chart from './Components/Chart';
+import './Styles/Dashboard.css'; 
+import { Link } from 'react-router-dom';
+import { faCogs } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+function Dashboard() {
+  return (
+    <div className='pageContainer'>
+        <NavBar />
+        <SideBar />
+
+        <Link to="/machines" className="dash-link">
+            <div className='machineCountStatus'>
+                <h1>Machines Assigned<FontAwesomeIcon icon={faCogs} size="2x" className="machineIcon" /></h1>
+                <h5 id='machinesCount'>3</h5>
+            </div>
+        </Link>
+
+        <div className='chartContainer'>
+            <Chart />
+        </div>
+        
+    </div>
+  );
+}
+
+export default Dashboard;
+
+
+{/* <Link to="/graphs" className="dash-link">
+        <div className='chartContainer'>
+            <Chart />
+        </div>
+      </Link> */}
