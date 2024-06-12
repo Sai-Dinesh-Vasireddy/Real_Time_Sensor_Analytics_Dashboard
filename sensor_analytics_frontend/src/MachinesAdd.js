369b22d4af32463c9e4e0d1f68ea5dd543bfc4d7
DineshVasireddy <saidinesh.vasireddy@slu.edu>
2024-06-12 16:53:42 -0500

    Fixed the issue with test cases

--- sensor_analytics_frontend/src/MachinesAdd.js
+++ sensor_analytics_frontend/src/MachinesAdd.js
@@ -1,106 +1,139 @@
-import { useState, useEffect, useRef } from "react";
-import { Line } from 'react-chartjs-2';
-import useWebSocket from 'react-use-websocket';
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    TimeScale,
-} from 'chart.js';
-import 'chartjs-adapter-date-fns';
+import React, { useState, useContext, useEffect } from 'react';
+import NavBar from './Components/NavBar';
+import SideBar from './Components/SideBar';
+// import { Link } from 'react-router-dom';
+import { UserContext } from './UserContext';
+import './Styles/MachinesAddPage.css';
+import { onboardNewSensor, assignMachineToUser, getAllMachines } from './api';
 
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    TimeScale
-);
+const MachinesAdd = () => {
+    const { user } = useContext(UserContext);
+    const [isLoading, setIsLoading] = useState(true);
+    const [groupName, setGroupName] = useState('');
+    const [topicName, setTopicName] = useState('');
+    const [userName, setUserName] = useState('');
+    const [machineNames, setMachineNames] = useState([]);
+    const [topicNames, setTopicNames] = useState([]);
+    const [selectedGroup, setSelectedGroup] = useState('');
+    const [selectedTopic, setSelectedTopic] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
-const Chart = ({ groupName, topicName }) => {
-    const [useWebsocketData, setUseWebsocketData] = useState([]);
-    const [chartData, setChartData] = useState({
-        labels: [],
-        datasets: [],
-    });
+    useEffect(() => {
+        if (user !== null) {
+          setIsLoading(false);
+          fetchData();
+        }
+    }, [user]);
 
-    const processMessages = (event) => {
+    const fetchData = async () => {
         try {
-            const parsedData = JSON.parse(event.data);
-            const timestamp = new Date();
+            const data = await getAllMachines(user.token);
+            setMachineNames(data.results.map(machine => machine.groupName));
+            setTopicNames(data.results.map(machine => machine.topicName));
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+    
+    if (isLoading) {
+        return <div>Loading...</div>; 
+    }
 
-            setUseWebsocketData((prevData) => {
-                const updatedData = [...prevData, { ...parsedData, timestamp }];
-                updateChart(updatedData);
-                return updatedData;
-            });
-        } catch (e) {
-            console.log("Data is not in format of Json");
+    const handleMachineAdd = async (event) => {
+        event.preventDefault();
+        setErrorMessage('');
+    
+        const machineName = `${groupName}_${topicName}`;
+
+        try {
+            await onboardNewSensor(groupName, topicName, machineName, user.token);
+            console.log('Machine added successfully');
+            // Reset input values
+            setGroupName('');
+            setTopicName('');
+            // Show success alert
+            alert('Machine added successfully');
+            // Fetch updated data
+            fetchData();
+        } catch (error) {
+            setErrorMessage(error.message);
         }
     };
 
-    const updateChart = (data) => {
-        const datasetNames = Object.keys(data[0]).filter(key => key !== 'timestamp');
+    const handleMachineAssign = async (event) => {
+        event.preventDefault();
+        setErrorMessage('');
 
-        setChartData({
-            labels: data.map(item => item.timestamp),
-            datasets: datasetNames.map(name => ({
-                label: name,
-                data: data.map(item => item[name]),
-                borderColor: 'rgba(75, 192, 192, 1)',
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            })),
-        });
-    };
+        const machineName = `${selectedGroup}_${selectedTopic}`;
 
-    const { lastJsonMessage } = useWebSocket(`ws://localhost:8080/topic?groupName=${groupName}&topicName=${topicName}`, {
-        onOpen: () => console.log('WebSocket connection opened.'),
-        onClose: () => console.log('WebSocket connection closed.'),
-        shouldReconnect: (closeEvent) => true,
-        reconnectInterval: 3000,
-        reconnectAttempts: 1,
-        onMessage: processMessages,
-    });
-
-    const options = {
-        scales: {
-            x: {
-                type: 'time',
-                time: {
-                    unit: 'second',
-                    tooltipFormat: 'HH:mm:ss',
-                    displayFormats: {
-                        second: 'HH:mm:ss'
-                    }
-                },
-                title: {
-                    display: true,
-                    text: 'Time'
-                }
-            },
-            y: {
-                title: {
-                    display: true,
-                    text: 'Values'
-                }
-            }
+        try {
+            await assignMachineToUser(userName, machineName, user.token);
+            console.log('Machine assigned successfully');
+            // Reset input values
+            setSelectedGroup('');
+            setSelectedTopic('');
+            setUserName('');
+            // Show success alert
+            alert('Machine assigned successfully');
+        } catch (error) {
+            setErrorMessage(error.message);
         }
     };
 
     return (
-        <div>
-            <h1 style={{color : "white"}}>Real-Time Machine Data Chart</h1>
-            <Line data={chartData} options={options} />
+        <div className='pageContainer'>
+            <NavBar />
+            <SideBar />
+            <div className='sensorDetails_Container'>
+                <input 
+                    name='groupName'
+                    value={groupName}
+                    placeholder='Enter Group Name'
+                    onChange={(e) => setGroupName(e.target.value)}
+                />
+
+                <input 
+                    name='topicName'
+                    value={topicName}
+                    placeholder='Enter Topic Name'
+                    onChange={(e) => setTopicName(e.target.value)}
+                />
+                
+                <button onClick={handleMachineAdd}> Add Machine </button>
+                {errorMessage && <div className="error">{errorMessage}</div>}
+            </div>
+
+            <div className='assignSensors_Container'>
+                <select id="groups" value={selectedGroup} onChange={(e) => setSelectedGroup(e.target.value)}>
+                    <option value="" disabled>Select a Group</option>
+                        {machineNames.map((machine, index) => (
+                        <option key={index} value={machine}>
+                            {machine}
+                    </option>
+                    ))}
+                </select>
+
+                <select id="topics" value={selectedTopic} onChange={(e) => setSelectedTopic(e.target.value)}>
+                    <option value="" disabled>Select a Topic</option>
+                        {topicNames.map((topic, index) => (
+                        <option key={index} value={topic}>
+                            {topic}
+                    </option>
+                    ))}
+                </select>
+
+                <input 
+                    name='Username'
+                    value={userName}
+                    placeholder='Enter Username'
+                    onChange={(e) => setUserName(e.target.value)}
+                />
+                
+                <button onClick={handleMachineAssign}> Assign Machine to User </button>
+                {errorMessage && <div className="error">{errorMessage}</div>}
+            </div>
         </div>
     );
-};
+}
 
-export default Chart;
+export default MachinesAdd;
